e5dfcc6f2dae9f3d42299ff84c6e941e689b7786
rajat-tech-22 <kumarajat7942@gmail.com>
2026-03-25 15:12:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def plot_bar(f, col, top_value, manager_name, key_val):
 def colored_metric(label, value, color="#2596be"):
     st.markdown(f"""
         <div style="
-            background-color: white;
+            background-color: #dea1ed;
             padding: 20px;
             border-radius: 12px;
             border-left: 6px solid {color};
