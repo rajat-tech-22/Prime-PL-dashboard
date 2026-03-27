1a0b7deac4657ec00be09cce8733f05995da8b1c
rajat-tech-22 <kumarajat7942@gmail.com>
2026-03-27 11:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ st.markdown("""
 # -----------------------------
 @st.cache_data(ttl=60)
 def load_data():
-    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfDuP8FJ8yxoZxPPiKYrxWyP1Brlq7hmyxrkp81_oURiPrCJHqup7Ru8wFoE-pWMEZjZIuH_VBM5_i/pub?output=csv
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfDuP8FJ8yxoZxPPiKYrxWyP1Brlq7hmyxrkp81_oURiPrCJHqup7Ru8wFoE-pWMEZjZIuH_VBM5_i/pub?output=csv"
     df.replace("null", None, inplace=True)
     return df
 
