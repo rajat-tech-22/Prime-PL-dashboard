15a51b6c6adee076d8361b87c66495cc5b7117c3
rajat-tech-22 <kumarajat7942@gmail.com>
2026-03-30 14:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ if dashboard_type == "All Managers":
 
         st.markdown(f"""
         <div style="
-            background-color: #F0F2F6;
+            background-color: Black;
             padding: 15px;
             border-radius: 10px;
             margin-bottom: 20px;
