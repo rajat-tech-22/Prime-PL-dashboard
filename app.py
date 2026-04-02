de8742a79dabcd09cc4878c9884462ad4f095ed0
rajat-tech-22 <kumarajat7942@gmail.com>
2026-04-02 14:11:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -813,7 +813,7 @@ if dashboard_type == "Prefr & PW Campaign Reports":
     delivered = int(filtered["RCS Delivered"].sum())
     read = int(filtered["RCS Read"].sum())
     clicks = int(filtered["RCS Unique Clicks"].sum())
-    cost =int(filtered["Cost"].sum())
+    cost =int(filtered["RCS Cost"].sum())
     total_disbursed = int(filtered["Disbursed"].sum())
     arg_ctr = round((clicks / delivered * 100) if delivered else 0, 2)
 
